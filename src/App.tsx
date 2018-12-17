f1f1598efc6787ba640f89d3faf806ab51d7570d
nrjshka <nrjshka@gmail.com>
2018-12-17 19:01:48 +0300

    Added: ts create react app start

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div>
+        Тест
+      </div>
+    );
+  }
+}
+
+export default App;
